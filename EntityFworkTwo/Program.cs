9f82e41cd0b87d2a7a5f39dc5e353b20820d0b64
ms.alievaalya@gmail.com <ms.alievaalya@gmail.com>
2023-02-03 20:37:21 +0300

    Добавьте файлы проекта.

---
+++ EntityFworkTwo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFworkTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AddFruit(new fandv(51, "name51", "fruit", "orange", 25));
+            GetAllVegetables();
+            
+            Console.ReadLine();
+        }
+        static void AddFruit(fandv item) //принимает таблицу, добавляет объект
+        {
+            using (FruitsAndVegetablesEntities fandvEntities = new FruitsAndVegetablesEntities()) //название есть в Context.cs
+            {
+                //x - пустая переменная, пока не обращается к dataset
+                fandv exist = fandvEntities.fandv.Where((x) => x.Id == item.Id && x.Name == item.Name).FirstOrDefault();
+                if (exist == null)
+                {
+                fandvEntities.fandv.Add(item); //item - сущность класса таблицы  
+                fandvEntities.SaveChanges();
+
+                }
+            }
+        }
+        static void GetAllVegetables()
+        {
+            using (FruitsAndVegetablesEntities fandvEntities = new FruitsAndVegetablesEntities())
+            {
+                List<fandv> list = fandvEntities.fandv.ToList();
+                //foreach (fandv item in fandvEntities.fandv) ;
+                foreach (fandv item in list)
+                {
+                    if (item.Type == "vegetables")
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+            }
+        }
+    }
+}
